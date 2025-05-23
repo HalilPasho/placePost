b9673d8f45fede01dd270c4ca21671b3587ec6bf
HalilPasho <halilpasho1@gmail.com>
2025-05-23 22:55:01 +0200

    cors in netlify

--- BE/app.js
+++ BE/app.js
@@ -12,16 +12,24 @@ const userRoute = require('./routes/usersRoute');
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Origin, X-Requested-With, Accept'
-  );
-  next();
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://placepst.netlify.app'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
 });
 
 app.use(bodyParser.json());
@@ -32,32 +40,32 @@ app.use('/places', placesRoute);
 app.use('/user', userRoute);
 
 app.use((req, res, next) => {
-  throw new httpError('Page router was not found', 404);
+    throw new httpError('Page router was not found', 404);
 });
 
 app.use((error, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
-  }
-  if (res.headersSent) {
-    return next(error);
-  }
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            console.log(err);
+        });
+    }
+    if (res.headersSent) {
+        return next(error);
+    }
 
-  res.status(error.status || 500).json({
-    message: error.message || 'Something went wrong',
-  });
+    res.status(error.status || 500).json({
+        message: error.message || 'Something went wrong',
+    });
 });
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
